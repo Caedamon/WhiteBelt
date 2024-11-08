92bff67c03954778dc15bb297b1ff0a0c922dc6c
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-11-08 11:18:31 +0100

    Exam Kata
    Break Time!

--- Exam Kata/Program.cs
+++ Exam Kata/Program.cs
@@ -11,19 +11,24 @@
 //     Check if either character’s health is 0 or below after each action and end the game if so.
 //     Display a win or loss message depending on the outcome.
 
-public class charValues
+public class CharValues
 {
     public string name;
     public int healthPoints;
     public float attackPower;
-    public bool isParalyzed;
-    public double currency;
-
-    public void PrintBase()
+    public int dodgeChance;
+    
+    public void PrintStats()
     {
         Console.WriteLine("Name: {0}", name);
         Console.WriteLine("Health: {0}", healthPoints);
         Console.WriteLine("Attack Power: {0}", attackPower);
+        Console.WriteLine("Dodge Chance: {0}", dodgeChance);
+    }
+
+    public bool TryDodge(Random random)
+    {
+        return random.Next(1,101) <= dodgeChance;
     }
 }
 
@@ -31,42 +36,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        charValues Hero = new charValues();
+        Random random = new Random();
+        
+        CharValues Hero = new CharValues();
         Hero.name = "Jim";
         Hero.healthPoints = 80;
-        Hero.attackPower = 7.5f;
+        Hero.attackPower = 15f;
+        Hero.dodgeChance = 10;
         
-        charValues Goblin = new charValues();
-        Goblin.name = "Goblin King";
+        CharValues Goblin = new CharValues();
+        Goblin.name = "Goblin";
         Goblin.healthPoints = 50;
-        Goblin.attackPower = 5f;
+        Goblin.attackPower = 10f;
+        Goblin.dodgeChance = 30;
 
-        Hero.PrintBase();
-        Goblin.PrintBase();
+        Hero.PrintStats();
+        Goblin.PrintStats();
         Console.WriteLine("Next");
         Console.ReadKey();
         Console.Clear();
         
-        Console.WriteLine("You are beeing attacked by the Goblin King! Defeat him!");
-        Console.WriteLine("1. Attack");
-        Console.WriteLine("2. Heal");
-        Console.Write("> ");
-        string choise = Console.ReadLine();
+        Console.WriteLine("You are beeing attacked by a Goblin! Defeat it!");
 
-        if (choise == "1")
+        while (Hero.healthPoints > 0 && Goblin.healthPoints > 0)
         {
-            Console.WriteLine("You Attack!");
+            Console.WriteLine("\nCurrent Stats:");
+            Hero.PrintStats();
+            Goblin.PrintStats();
+            
+            Console.WriteLine("Chose an action!");
+            Console.WriteLine("1. Attack");
+            Console.WriteLine("2. Heal");
+            Console.Write("> ");
+            string choise = Console.ReadLine();
+
+            if (choise == "1")
+            {
+                Console.WriteLine("You Attack!");
+                if (Goblin.TryDodge(random))
+                {
+                    Console.WriteLine("The Goblin Dodges your pathetic attempt at an attack!");
+                }
+                else
+                {
+                    Goblin.healthPoints -= (int)Hero.attackPower;
+                    Console.WriteLine($"Goblin takes {Hero.attackPower} damage!");
+                }
+            }
+            else if (choise == "2")
+            {
+                int healAmount = 10;
+                Hero.healthPoints += healAmount;
+                Console.WriteLine($"You Heal yourself for {healAmount} health!!");
+            }
+            else
+            {
+                Console.WriteLine($"{choise}??? the hell is that! Try again! 1, or 2 damnit!");
+            }
+            
+            Console.WriteLine("");
+            if (Hero.TryDodge(random))
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
         }
-        else if (choise == "2")
-        {
-            Console.WriteLine("You Heal yourself!!");
-        }
-        else
-        {
-            Console.WriteLine($"{choise}??? the hell is that! Try again! 1, 2 or 3 damnit!");
-        }
-        Console.WriteLine("Press any key to quit...");
-        Console.ReadKey();
     }
 }
 
